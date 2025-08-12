0ff6e54a5e34fb552eecf8df508d62b3c890aa53
Aiman Rehan <134811067+Arehan19@users.noreply.github.com>
2025-08-12 16:04:59 +0500

    Delete data.js

--- data.js
+++
@@ -1,9 +0,0 @@
-const getDeviceInfo = () => {
-  return {
-    url: window.location.href,
-    userAgent: navigator.userAgent,
-    viewport: `${window.innerWidth}x${window.innerHeight}`,
-  };
-};
-
-export default getDeviceInfo;
